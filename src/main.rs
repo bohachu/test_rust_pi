55a4359f6719a8c51ed8946404a272d6c2cd8c0e
bohachu <cbh@cameo.tw>
2023-03-27 21:29:47 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,12 @@ fn chudnovsky(digits: i32) -> f64 {
     let k = ((digits as f64) / 14.181647462725477).ceil() as i32;
     let mut sum = 0.0;
     for i in 0..k {
-        let num = factorial(6 * i) * (13591409 + 545140134 * i) as f64;
-        let denom = factorial(3 * i) * (factorial(i) as f64).powi(3) * (-262537412640768000).pow(i);
+        let num = factorial(6 * i) as f64 * (13591409 + 545140134 * i) as f64;
+        let denom = factorial(3 * i) as f64 * (factorial(i) as f64).powi(3) * (-262537412640768000).pow(i);
         sum += num / denom;
     }
-    let pi = 426880.0 * ((10005.0).sqrt() / sum);
-    pi.round() / 10_f64.powi(digits - 1) as f64
+    let pi = 426880.0 * ((10005.0_f64).sqrt() / sum);
+    (pi * 10_f64.powi(digits - 1)).round() / 10_f64.powi(digits - 1)
 }
 
 fn factorial(n: i32) -> i32 {
